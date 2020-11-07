cd42b1c46d3bdfd2feb859e1ea96750b7d5ccf43
Jan Sommer <jan-sommer@outlook.com>
2020-11-07 21:23:03 +0100

    First draft of recipe list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import recipe01 from '@assets/sample-data/recipe-01.json';
+import recipe02 from '@assets/sample-data/recipe-02.json';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public readonly exampleRecipes = [
+    recipe01,
+    recipe02
+  ];
 }
