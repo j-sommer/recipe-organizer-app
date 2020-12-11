bf66446c1f3808b50053dd98c60f66f8abd458bb
Jan Sommer <jan-sommer@outlook.com>
2020-12-11 06:22:53 +0100

    Create page for category management

---
+++ src/app/categories/categories.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { CategoriesPageRoutingModule } from './categories-routing.module';
+import { CategoriesPage } from './categories.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    CategoriesPageRoutingModule,
+    TranslateModule.forChild(),
+  ],
+  declarations: [CategoriesPage],
+})
+export class CategoriesPageModule {}
